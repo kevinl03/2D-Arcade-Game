0f1464f51cfd1a9e88fc4a6a2fefa28cfcb8b0b2
Sadaf Ahmadi <saa142@sfu.ca>
2022-10-29 12:25:26 -0700

    rewards

--- Code/project/src/main/java/Entities/Bonus.java
+++ Code/project/src/main/java/Entities/Bonus.java
@@ -1,5 +1,11 @@
 package Entities;
 
-public class Bonus extends Reward{
-    
+public class Bonus extends Reward {
+    public Bonus() {
+        super();
+    }
+
+    public Bonus(int x, int y, int time, int p) {
+        super(x, y, time, p);
+    }
 }
--- Code/project/src/main/java/Entities/RegularReward.java
+++ Code/project/src/main/java/Entities/RegularReward.java
@@ -1,5 +1,10 @@
-package project.src.main.java.Entities;
+package Entities;
 
 public class RegularReward extends Reward {
-    
+    public  RegularReward(){
+        super();
+    }
+    public RegularReward(int x, int y, int time, int p){
+        super(x,y,time,p);
+    }
 }
