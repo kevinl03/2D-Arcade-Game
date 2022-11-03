cd7004a534af02f486875eed1598c0d246b77c31
Kevin Litvin <ktl13@sfu.ca>
2022-11-03 14:37:02 -0700

    BoardData no longer gets stuck in infinite loop when generating many walls (works for 1 million random generations) Fixed bug that had an extra bracket in EnemyLogic.java

--- Code/project/src/main/java/Logic/EnemyLogic.java
+++ Code/project/src/main/java/Logic/EnemyLogic.java
@@ -16,7 +16,7 @@ import java.util.Queue;
 
 public class EnemyLogic {
 
-    public void processEnemyMovement(ObjectData gameObjectData){
+    public void processEnemyMovement(ObjectData gameObjectData) {
 
         BoardData board = gameObjectData.getBoard();
         GameStats gameStats = gameObjectData.getGameStats();
@@ -29,8 +29,8 @@ public class EnemyLogic {
 
         Objects[][] tempArr = new Objects[width][height];
 
-        for(int i = 0; i < width; i++){
-            for(int j = 0; j < height; j++){
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 tempArr[i][j] = boardArr[i][j];
             }
         }
@@ -40,13 +40,13 @@ public class EnemyLogic {
 
 //            System.out.println(nextMove);
 
-            if(nextMove != Direction.NULL){
+            if (nextMove != Direction.NULL) {
                 Objects currentTile = board.getTypeAt(enemy);
-                if(currentTile == Objects.ENEMYANDREWARD){
+                if (currentTile == Objects.ENEMYANDREWARD) {
                     board.setTypeAt(enemy, Objects.REWARD);
-                }else if(currentTile == Objects.ENEMYANDTRAP){
+                } else if (currentTile == Objects.ENEMYANDTRAP) {
                     board.setTypeAt(enemy, Objects.TRAP);
-                }else{
+                } else {
                     board.setTypeAt(enemy, Objects.EMPTY);
                 }
             }
@@ -69,15 +69,15 @@ public class EnemyLogic {
 
             }
 
-            if(nextMove != Direction.NULL){
+            if (nextMove != Direction.NULL) {
                 Objects currentTile = board.getTypeAt(enemy);
-                if(currentTile == Objects.REWARD){
+                if (currentTile == Objects.REWARD) {
                     board.setTypeAt(enemy, Objects.ENEMYANDREWARD);
-                }else if(currentTile == Objects.TRAP){
+                } else if (currentTile == Objects.TRAP) {
                     board.setTypeAt(enemy, Objects.ENEMYANDTRAP);
-                }else if(currentTile == Objects.HERO){
+                } else if (currentTile == Objects.HERO) {
                     gameStats.setGameOver(true);
-                }else{
+                } else {
                     board.setTypeAt(enemy, Objects.ENEMY);
                 }
             }
@@ -87,7 +87,7 @@ public class EnemyLogic {
 
     //findShortestPath runs a bfs on a 2d char array to find the direction the enemy at enemyPos
     //needs to move in order to get to the hero with the shortest distance
-    public Direction findShortestPath(Objects[][] board, Position enemyPos){
+    public Direction findShortestPath(Objects[][] board, Position enemyPos) {
         int width = board.length;
         int height = board[0].length;
 
@@ -96,14 +96,13 @@ public class EnemyLogic {
         Node start = new Node(enemyPos, Direction.NULL, 0);
         q.add(start);
 
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             Node head = q.poll();
 
             //'h' represents hero on the board
-            if(board[head.getX()][head.getY()] == Objects.HERO){
+            if (board[head.getX()][head.getY()] == Objects.HERO) {
                 return head.initialDirection;
-            }
-            else{
+            } else {
                 //Objects.TREE represents tree on the board, I swap a visited node to a tree
                 //so that the bfs knows not to search the head node again.
                 board[head.getX()][head.getY()] = Objects.TREE;
@@ -117,41 +116,41 @@ public class EnemyLogic {
     }
 
     //getNeighbours returns a list containing all the direct neighbours to head
-    private List<Node> getNeighbours(Node head, Objects[][] board, int width, int height){
+    private List<Node> getNeighbours(Node head, Objects[][] board, int width, int height) {
         int x = head.getX();
         int y = head.getY();
 
         List<Node> neighbours = new LinkedList<>();
         //check if there is a non tree tile in each direction (North, East, South, West), if so, add it to neighbours list
         //only change the initialDirection if it is the initial node from where the search started (the enemy)
-dddd            if(head.initialDirection == Direction.NULL){
-                neighbours.add(new Node(new Position(x + 1, y), Direction.EAST, head.pathLength+1));
-            }else{
-                neighbours.add(new Node(new Position(x + 1, y), head.initialDirection, head.pathLength+1));
+        if (head.initialDirection == Direction.NULL) {
+            neighbours.add(new Node(new Position(x + 1, y), Direction.EAST, head.pathLength + 1));
+        } else {
+            neighbours.add(new Node(new Position(x + 1, y), head.initialDirection, head.pathLength + 1));
+        }
+
+
+        if (x - 1 >= 0 && x - 1 < width && board[x - 1][y] != Objects.TREE) {
+            if (head.initialDirection == Direction.NULL) {
+                neighbours.add(new Node(new Position(x - 1, y), Direction.WEST, head.pathLength + 1));
+            } else {
+                neighbours.add(new Node(new Position(x - 1, y), head.initialDirection, head.pathLength + 1));
             }
         }
 
-        if(x - 1 >= 0 && x - 1 < width && board[x - 1][y] != Objects.TREE){
-            if(head.initialDirection == Direction.NULL){
-                neighbours.add(new Node(new Position(x - 1, y), Direction.WEST, head.pathLength+1));
-            }else{
-                neighbours.add(new Node(new Position(x - 1, y), head.initialDirection, head.pathLength+1));
+        if (y + 1 >= 0 && y + 1 < height && board[x][y + 1] != Objects.TREE) {
+            if (head.initialDirection == Direction.NULL) {
+                neighbours.add(new Node(new Position(x, y + 1), Direction.NORTH, head.pathLength + 1));
+            } else {
+                neighbours.add(new Node(new Position(x, y + 1), head.initialDirection, head.pathLength + 1));
             }
         }
 
-        if(y + 1 >= 0 && y + 1 < height && board[x][y + 1] != Objects.TREE){
-            if(head.initialDirection == Direction.NULL){
-                neighbours.add(new Node(new Position(x, y + 1), Direction.NORTH, head.pathLength+1));
-            }else{
-                neighbours.add(new Node(new Position(x, y + 1), head.initialDirection, head.pathLength+1));
-            }
-        }
-
-        if(y - 1 >= 0 && y - 1 < height && board[x][y - 1] != Objects.TREE){
-            if(head.initialDirection == Direction.NULL){
-                neighbours.add(new Node(new Position(x, y - 1), Direction.SOUTH, head.pathLength+1));
-            }else{
-                neighbours.add(new Node(new Position(x, y - 1), head.initialDirection, head.pathLength+1));
+        if (y - 1 >= 0 && y - 1 < height && board[x][y - 1] != Objects.TREE) {
+            if (head.initialDirection == Direction.NULL) {
+                neighbours.add(new Node(new Position(x, y - 1), Direction.SOUTH, head.pathLength + 1));
+            } else {
+                neighbours.add(new Node(new Position(x, y - 1), head.initialDirection, head.pathLength + 1));
             }
         }
 
