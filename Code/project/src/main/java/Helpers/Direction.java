eb78cc41c3f08f3dace16222e8d29e67065a8889
Misha Goldenberg <mgoldenb@sfu.ca>
2022-11-03 22:47:25 -0700

    added toString method to direction enum

--- Code/project/src/main/java/Helpers/Direction.java
+++ Code/project/src/main/java/Helpers/Direction.java
@@ -1,5 +1,25 @@
 package Helpers;
 
 public enum Direction {
-    NORTH, EAST, SOUTH, WEST, NULL
+    NORTH{
+        @Override
+        public String toString() {
+            return "North";
+        }
+    }, EAST{
+        @Override
+        public String toString() {
+            return "East";
+        }
+    }, SOUTH{
+        @Override
+        public String toString() {
+            return "South";
+        }
+    }, WEST{
+        @Override
+        public String toString() {
+            return "West";
+        }
+    }, NULL
 }
