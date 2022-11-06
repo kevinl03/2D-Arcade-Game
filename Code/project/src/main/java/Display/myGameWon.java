ddd97da85344d25b798fe26724d46f949e08c65d
Kevin Litvin <ktl13@sfu.ca>
2022-11-05 23:58:41 -0700

    game won screen background image set

--- Code/project/src/main/java/Display/myGameWon.java
+++ Code/project/src/main/java/Display/myGameWon.java
@@ -1,9 +1,12 @@
 package Display;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class myGameWon extends JPanel {
     private JLabel gameLabel;
@@ -13,26 +16,35 @@ public class myGameWon extends JPanel {
     private Font titleText;
     private Font headerText;
 
+    private BufferedImage win_png;
+
+
     public myGameWon(DisplayLayout dl, CardLayout cl){
         titleText = new Font("Times New Roman", Font.BOLD, 50);
         headerText = new Font("Times New Roman", Font.BOLD, 30);
         this.setLayout(null);
         gameLabel = new JLabel("CONGRATULATIONS");
         gameLabel.setFont(titleText);
-        gameLabel.setBounds(500, 0, 550, 100);
+        gameLabel.setBounds(500, 300, 550, 100);
         timeLabel = new JLabel();
         timeLabel.setFont(headerText);
-        timeLabel.setBounds(700, 100, 800, 100);
+        timeLabel.setBounds(700, 400, 800, 100);
         scoreLabel = new JLabel();
         scoreLabel.setFont(headerText);
-        scoreLabel.setBounds(700, 200, 800, 100);
+        scoreLabel.setBounds(700, 500, 800, 100);
         this.add(gameLabel);
         this.add(timeLabel);
         this.add(scoreLabel);
 
+        try {
+            win_png = ImageIO.read(getClass().getResource("/win.png"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         gomenuButton = new JButton("Main Menu");
         gomenuButton.setFocusable(false);
-        gomenuButton.setBounds(650, 400, 200, 100);
+        gomenuButton.setBounds(700, 600, 200, 100);
         // Game over screen, add menu button
         this.add(gomenuButton);
 
@@ -54,6 +66,13 @@ public class myGameWon extends JPanel {
 
             }
         });
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (win_png != null) {
+            g.drawImage(win_png, 0, 0, 1500, 960, null);
+        }
     }
 
     public void setValues(int time, int score) {
