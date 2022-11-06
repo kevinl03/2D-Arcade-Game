7567bbef8844437d418696421167bf9323a5f619
Misha Goldenberg <mgoldenb@sfu.ca>
2022-11-05 18:25:00 -0700

    added documentation to Position, MovingEntities, Hero , and Enemy

--- Code/project/src/main/java/Entities/Enemy.java
+++ Code/project/src/main/java/Entities/Enemy.java
@@ -1,7 +1,16 @@
 package Entities;
 
+
+/**
+ * Represents the Enemy in game
+ */
 public class Enemy extends MovingEntity {
 
+    /**
+     * Constructs enemy at x,y
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     public Enemy(int x,int y){
         super(x,y);
     }
--- Code/project/src/main/java/Entities/MovingEntity.java
+++ Code/project/src/main/java/Entities/MovingEntity.java
@@ -2,13 +2,29 @@ package Entities;
 
 import Helpers.Direction;
 
-public class MovingEntity extends Position {
-    protected int currentZone;
 
+/**
+ * Represents all non-static entities in game
+ */
+public class MovingEntity extends Position {
+    /**
+     * Direction that the moving entitiy is facing
+     */
     private Direction dir;
+
+    /**
+     * Moving status of the entity (stationary or moving)
+     */
     private boolean moving;
 
+    /**
+     * The current frame in the moving animation of the entity
+     */
     private int animationFrame;
+
+    /**
+     * Default Constructor
+     */
     public MovingEntity(){
         dir = Direction.NORTH;
         animationFrame = 1;
@@ -16,6 +32,12 @@ public class MovingEntity extends Position {
 
     }
 
+
+    /**
+     * Constructs a moving entity at x,y
+     * @param x x coordinate
+     * @param y y coordinate
+     */
     public MovingEntity(int x, int y){
         this.x = x;
         this.y = y;
@@ -24,26 +46,26 @@ public class MovingEntity extends Position {
         moving = false;
     }
 
-    public int getCurrentZone() {
-        return currentZone;
-    }
 
-    public void setCurrentZone(int currentZone) {
-        this.currentZone = currentZone;
-    }
-
+    /**
+     * Gets the direction
+     * @return The dir
+     */
     public Direction getDir() {
         return dir;
     }
 
+    /**
+     * Gets the current animation frame
+     * @return The animation frame
+     */
     public int getAnimationFrame() {
         return animationFrame;
     }
 
-    public void setAnimationFrame(int animationFrame) {
-        this.animationFrame = animationFrame;
-    }
-
+    /**
+     * Increments the animation frame by 1
+     */
     public void incrementAnimationFrame(){
         this.animationFrame++;
         if(animationFrame > 3){
@@ -51,15 +73,29 @@ public class MovingEntity extends Position {
         }
     }
 
+
+    /**
+     * sets the entities dir if not null
+     * @param dir New direction
+     */
     public void setDir(Direction dir) {
         if(dir != Direction.NULL){
             this.dir = dir;
         }
     }
+
+    /**
+     * Sets the moving status
+     * @param moving New moving status
+     */
     public void setMoving(boolean moving){
         this.moving = moving;
     }
 
+    /**
+     * Gets the moving status of entity
+     * @return Moving status
+     */
     public boolean isMoving() {
         return moving;
     }
