49020450191f30bf3cb245f03f13fff169c6aa8d
Misha Goldenberg <mgoldenb@sfu.ca>
2022-11-05 18:24:12 -0700

    added documentation to ObjectData and GameStats + removed unused code

--- Code/project/src/main/java/Game/GameStats.java
+++ Code/project/src/main/java/Game/GameStats.java
@@ -1,29 +1,47 @@
 package Game;
 
-import Helpers.Stopwatch;
-
+/**
+ * Represents game loss and game won statistics
+ */
 public class GameStats {
+    /**
+     * game over value
+     */
     private boolean gameOver = false;
 
+    /**
+     * game won value
+     */
     private boolean gameWon = false;
-    private Stopwatch time = new Stopwatch();
 
-
-    public Stopwatch getTime() {
-        return time;
-    }
-
+    /**
+     * Sets the game over value
+     * @param gameOver new game over value
+     */
     public void setGameOver(boolean gameOver) {
         this.gameOver = gameOver;
     }
 
+    /**
+     * Gets game over value
+     */
     public boolean getGameOver() {
         return gameOver;
     }
 
+
+    /**
+     * Gets game won value
+     * @return Game won value
+     */
     public boolean isGameWon() {
         return gameWon;
     }
+
+    /**
+     * Sets game won value
+     * @param gameWon New game won value
+     */
     public void setGameWon(boolean gameWon) {
         this.gameWon = gameWon;
     }
