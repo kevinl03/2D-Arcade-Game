97069bb1e525e0e1641f29e5ffc5e514ceb54374
Kevin Litvin <ktl13@sfu.ca>
2022-11-15 16:51:48 -0800

    Created higherarchy for testing.
    Tests will be under src/test/java
    
    used junit.jupiter for library within src/test/resources

---
+++ Code/project/src/test/java/MapGenerationTest.java
@@ -0,0 +1,19 @@
+
+import org.junit.jupiter.api.Test;
+
+public class MapGenerationTest {
+    @Test
+    void wallgen(){
+
+        assert(true);
+    }
+    @Test
+    void bonusreward(){
+        assert(true);
+    }
+    @Test
+    void entitygen(){
+        //System.out.printf("ur fat");
+        assert(1 < 2);
+    }
+}
