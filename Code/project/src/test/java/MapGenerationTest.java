e08fb9984e8e1e47bada4a925980f5561915ba31
Misha Goldenberg <misha_goldenberg@sfu.ca>
2022-11-23 11:59:08 -0800

    minimum proximity tests for rewards and enenmy spawning

--- Code/project/src/test/java/MapGenerationTest.java
+++ Code/project/src/test/java/MapGenerationTest.java
@@ -2,15 +2,127 @@
 import com.Board.BoardData;
 import com.Board.Difficulty;
 import com.Board.Objects;
+import com.Entities.Position;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 public class MapGenerationTest {
     @Test
-    void wallgen() {
-
-        assert (true);
+    void OuterBoarderGenerationTest() {
+        for (Difficulty dif : Difficulty.values()) {
+            BoardData board = new BoardData();
+            board.initialiseBoard(dif);
+            int x = board.getboardwidth();
+            int y = board.getboardheight();
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    if(board.getTypeAt(i,j) != Objects.EXIT){
+                        if( i == 0 || i == x-1){
+                            Assertions.assertEquals(Objects.TREE, board.getTypeAt(i,j));
+                        }else if (j == 0 || j == y-1){
+                            Assertions.assertEquals(Objects.TREE, board.getTypeAt(i,j));
+                        }
+                    }
+                }
+            }
+        }
     }
+    @Test
+    void MinDistanceBetweenEnemyAndHero() {
+        for (Difficulty dif : Difficulty.values()) {
+            int minProximity = 0;
+            switch(dif){
+                case EASY: minProximity = 10;
+                    break;
+                case MEDIUM: minProximity = 7;
+                    break;
+                case HARD: case INFINITE: minProximity = 5;
+                    break;
+            }
+
+            BoardData board = new BoardData();
+            board.initialiseBoard(dif);
+            int x = board.getboardwidth();
+            int y = board.getboardheight();
+
+            ArrayList<Position> enemyPositions = new ArrayList<>();
+            int heroX = 0;
+            int heroY = 0;
+
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    Objects typeAtTile = board.getTypeAt(i,j);
+
+                    if(typeAtTile == Objects.HERO){
+                        heroX = i;
+                        heroY = j;
+                    } else if (typeAtTile == Objects.ENEMY) {
+                        enemyPositions.add(new Position(i,j));
+                    }
+                }
+            }
+
+            for(Position enemyPos : enemyPositions){
+                int enemyX = enemyPos.getX();
+                int enemyY = enemyPos.getY();
+
+                double distance = Math.sqrt(Math.pow(Math.abs(heroX -enemyX),2) + Math.pow(Math.abs(heroY - enemyY), 2));
+
+                Assertions.assertTrue(distance >= minProximity);
+            }
+        }
+
+    }
+
+    @Test
+    void MinDistanceBetweenRewards() {
+        for (Difficulty dif : Difficulty.values()) {
+            int minProximity = 0;
+            switch(dif){
+                case EASY: minProximity = 5;
+                    break;
+                case MEDIUM: minProximity = 3;
+                    break;
+                case HARD: case INFINITE: minProximity = 0;
+                    break;
+            }
+
+            BoardData board = new BoardData();
+            board.initialiseBoard(dif);
+            int x = board.getboardwidth();
+            int y = board.getboardheight();
+
+            ArrayList<Position> rewardPositions = new ArrayList<>();
+
+            for (int i = 0; i < x; i++) {
+                for (int j = 0; j < y; j++) {
+                    Objects typeAtTile = board.getTypeAt(i,j);
+
+                    if (typeAtTile == Objects.REWARD) {
+                        rewardPositions.add(new Position(i,j));
+                    }
+                }
+            }
+
+            for(Position reward : rewardPositions){
+                for(Position reward2 : rewardPositions){
+                    int rewardX = reward.getX();
+                    int rewardY = reward.getY();
+                    int reward2X = reward2.getX();
+                    int reward2Y = reward2.getY();
+                    double distance = Math.sqrt(Math.pow(Math.abs(rewardX -reward2X),2) + Math.pow(Math.abs(rewardY - reward2Y), 2));
+                    Assertions.assertTrue(distance >= minProximity || distance == 0);
+                }
+
+
+            }
+        }
+
+    }
+
+
 
     @Test
     void bonusreward() {
