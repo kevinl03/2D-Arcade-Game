ba40866014dfd0d82e6f010f2122f9bd9e2232db
Misha Goldenberg <misha_goldenberg@sfu.ca>
2022-11-24 13:33:00 -0800

    changed resolution function to use toolKit

--- Code/project/src/main/java/com/Display/myTitle.java
+++ Code/project/src/main/java/com/Display/myTitle.java
@@ -68,7 +68,7 @@ public class myTitle extends JPanel {
         gbc.gridx = 0;
         gbc.gridy = 1;
         gbc.ipadx = 200;
-        gbc.ipady = 50;
+        gbc.ipady = dl.displayheight/100;
         playButton.setFocusable(false);
         playButton.setBorderPainted(false);
         playButton.setOpaque(false);
@@ -192,8 +192,9 @@ public class myTitle extends JPanel {
     @Override
     protected void paintComponent(Graphics g){
         super.paintComponent(g);
+
         g.drawImage(title_png, 0, 0, displayLayout.displaywidth, displayLayout.displayheight, null);
-        g.drawImage(title_squirrel, displayLayout.displaywidth/4, displayLayout.displayheight/2, 293, 400, null);
+        g.drawImage(title_squirrel, displayLayout.displaywidth/4, displayLayout.displayheight/2, displayLayout.displaywidth/4, displayLayout.displayheight/3, null);
     }
 
     public JButton getplayButton() {return playButton;}
