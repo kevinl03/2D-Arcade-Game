01b84997e1f6d117117db171b2d2d296631c51eb
gyc5 <gyc5@sfu.ca>
2022-11-26 19:31:54 -0800

    Disabled Playbutton test, Test NotVisible screen

--- Code/project/src/test/java/KeyHandlerTests.java
+++ Code/project/src/test/java/KeyHandlerTests.java
@@ -14,6 +14,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
+//@Disabled
 public class KeyHandlerTests {
     @Test
     void keyHandlerTest(){
